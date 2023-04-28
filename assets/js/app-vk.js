c3788efa3c79fc1480b9460db393a11ae327f903
Talgat <84556670+kambarbaev@users.noreply.github.com>
2023-04-28 15:44:55 +0600

    refactor: change html structure, add basic style and js

--- assets/js/app-vk.js
+++ assets/js/app-vk.js
@@ -1,1 +1,9 @@
-console.log('салам ))');
+const keyboard = document.querySelector('.keyboard');
+const outputField = document.querySelector('.page__textarea-field');
+
+keyboard.addEventListener('click', (event) => {
+  if (event.target.classList.contains('key')) {
+    const targetContent = event.target.textContent;
+    outputField.value += targetContent;
+  }
+});
