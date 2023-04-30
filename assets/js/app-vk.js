6a6a24e2bafdc374f9e72543578511945046a704
Talgat <akamypka@gmail.com>
2023-04-30 22:32:19 +0600

    refactor: change key dataset attribute, add eventlistenter for special buttons

--- assets/js/app-vk.js
+++ assets/js/app-vk.js
@@ -1,10 +1,31 @@
 const keyboard = document.querySelector('.keyboard');
 const outputField = document.querySelector('.page__textarea-field');
 const keyboardKeys = document.querySelectorAll('.key');
+const ignoredButtons = ['AltLeft', 'ControlLeft', 'AltRight', 'ControlRight'];
+// const arrowButtons = ['ArrowLeft', 'ArrowUp', 'ArrowDown', 'ArrowRight'];
 
 keyboard.addEventListener('mousedown', (event) => {
   if (event.target.classList.contains('key')) {
-    outputField.value += event.target.textContent;
+    if (ignoredButtons.includes(event.target.dataset.code)) {
+      return;
+    }
+    switch (event.target.dataset.key) {
+      case 'Space':
+        outputField.value += ' ';
+        break;
+      case 'Tab':
+        outputField.value += '\t';
+        break;
+      case 'Backspace':
+        outputField.value = outputField.value.slice(0, -1);
+        break;
+      case 'Enter':
+        outputField.value += '\n';
+        break;
+      default:
+        outputField.value += event.target.textContent;
+        break;
+    }
   }
 });
 
